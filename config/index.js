8dc9cc327209cda586703fcbdf969b3a81606c9a
Izhan-Mughal <izhanmughal11@gmail.com>
2023-01-27 00:22:39 +0500

    27

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev 
     ? "http://localhost:3000" 
-    : "https://foodonlines.netlify.app";
+    : "https://foodweb1.vercel.app/";
